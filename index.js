1c2801e59a3855c445afef83d98edfc60ae680bb
kevaldobariya25 <125066957+kevaldobariya25@users.noreply.github.com>
2024-05-26 13:17:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const db = new pg.Client({
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_DATABASE,
   password: process.env.DATABASE_PASSWORD,
-  port: 5432,
+  port: 5050,
 });
 
 db.connect();
